2c18c601ce737e3cb6aa1596224539157b163af0
Kevin Mooney <kmooney517@gmail.com>
2015-10-09 12:02:49 -0400

    added the gulp watch stuff that jd did in class

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp    = require('gulp');
+var sass    = require('gulp-sass');
+var plumber = require('gulp-plumber');
+var notify  = require('gulp-notify');
+
+var notifyError = function(){
+  return plumber({
+    errorHandler: notify.onError('Error: <%= error.message %>')
+  })
+};
+
+
+gulp.task('watch', function(){
+
+  gulp.watch('./scss/*.scss', ['sass']);
+
+});
+
+gulp.task('sass', function(){
+
+  gulp.src('./scss/*.scss')
+    .pipe( notifyError() )
+    .pipe( sass() )
+    .pipe( gulp.dest('./css') );
+
+});
